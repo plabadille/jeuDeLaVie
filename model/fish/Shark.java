1d060ac06820a1662f4a3ae8d0eefcdae537c0fd
AlexLulu <AlexLulu>
2016-12-01 17:29:29 +0100

    mise à jour des classes Shark, mer

--- model/fish/Shark.java
+++ model/fish/Shark.java
@@ -1,5 +1,26 @@
 package model.fish;
 
 public class Shark extends Fish {
-	//to do
+    
+    
+    
+    public Shark(int deadAge, int spawnAge, int spawnRatio, int x, int y)
+    {
+        super(deadAge, spawnAge, spawnRatio, x, y);
+    }
+
+    @Override
+    public void dead() {
+        
+    }
+
+    @Override
+    public void move() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+	
+    public void eatSardine()
+    {
+        //to do
+    }
 }
--- model/sea/Sea.java
+++ model/sea/Sea.java
@@ -1,5 +1,23 @@
 package model.sea;
 
+import model.fish.Fish;
+
 public class Sea {
-	//to do
+    
+	private int length;
+        private int width;
+        private Fish [][] sea ;
+        
+        
+        
+    public Sea(int length, int width, int startSardineNumber, int startSharkNumber)
+    {
+        this.length = length;
+        this.width = width;
+        
+        this.sea = new Fish[width][length];
+        
+    }
+
 }
+
