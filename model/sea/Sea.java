8a4210bff9f32e51a7310c32200ef4484867c675
plabadille <plabadille@gmail.com>
2017-01-06 23:30:31 +0100

    add method to get neigborhood, to move a fish and to delete a fish <- to be used by appropriate method in fish.behaviour

--- model/sea/Sea.java
+++ model/sea/Sea.java
@@ -2,6 +2,7 @@
 package model.sea;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import model.fish.*;
 import model.gameEngine.GameConstants;
@@ -9,23 +10,23 @@ import model.sea.populate.*;
 
 public class Sea {
 	//TO DO Une méthode voisinage par exemple pourrait renvoyer un tableau de 8 cases par exemple
-	private int length;
-    private int width;
+	private int row;
+    private int column;
     private Fish [][] sea ;
     private ArrayList<Fish> fishAlive;
         
     public Sea(GameConstants gc) {
     	
-        this.length = gc.getSeaLenght();
-        this.width = gc.getSeaWidth();
+        this.row = gc.getSeaLenght();
+        this.column = gc.getSeaWidth();
         
-        this.sea = new Fish[width][length];
+        this.sea = new Fish[column][row];
         this.fishAlive = new ArrayList<Fish>();
         
         populateSea(gc);
         
-        for(int x = 0; x < this.length; x++) {
-        	for(int y = 0; y < this.width; y++) {
+        for(int x = 0; x < this.column; x++) {
+        	for(int y = 0; y < this.row; y++) {
         		if (sea[x][y] != null) {
         			this.fishAlive.add(sea[x][y]);
         		}
@@ -36,7 +37,7 @@ public class Sea {
     
     private void populateSea(GameConstants gc) {
     	
-    	int seaSize = this.length * this.width;
+    	int seaSize = this.row * this.column;
     	int fishNb = gc.getSeaSharkNumber() + gc.getSeaSardineNumber();
     	float populationRate = fishNb / seaSize;
     	
@@ -62,6 +63,53 @@ public class Sea {
 	public void setSea(Fish[][] sea) {
 		this.sea = sea;
 	}
+	
+	public void moveFish(Fish fish, int x, int y) {
+		this.sea[x][y] = this.sea[fish.getCoordinateX()][fish.getCoordinateY()];
+		this.sea[fish.getCoordinateX()][fish.getCoordinateY()] = null;
+		fish.setCoordinateX(x);
+		fish.setCoordinateY(y);
+	}
+	
+	public void deleteFish(Fish fish) {
+		int x = fish.getCoordinateX();
+		int y = fish.getCoordinateY();
+		
+		this.sea[x][y] = null;
+		this.fishAlive.remove(fish);
+	}
+	
+	public HashMap<String,Fish> getNeighborhood(Fish fish) {
+		
+		int x = fish.getCoordinateX();
+		int y = fish.getCoordinateY();		
+		HashMap<String,Fish> neighborhood = new HashMap<String,Fish>();
+		
+		//We store the possible neighbor in a hashMap. If there is no neighbor in a possible move, we don't add the move.
+		if (x-1 >= 0) {
+			neighborhood.put("LEFT", this.sea[x-1][y]);
+			if (y-1 >= 0)
+				neighborhood.put("DOWN_LEFT", this.sea[x-1][y-1]);
+			if (y+1 <= this.row-1)
+				neighborhood.put("TOP_LEFT", this.sea[x-1][y+1]);
+		}
+		
+		if (x+1 <= this.column-1) {
+			neighborhood.put("RIGHT", this.sea[x+1][y]);
+			if (y-1 >= 0)
+				neighborhood.put("DOWN_RIGHT", this.sea[x+1][y-1]);
+			if (y+1 <= this.row-1)
+				neighborhood.put("TOP_RIGHT", this.sea[x+1][y+1]);
+		}
+		
+		if (y-1 >= 0)
+			neighborhood.put("DOWN", this.sea[x][y-1]);
+		if (y+1 <= this.row-1)
+			neighborhood.put("TOP", this.sea[x][y+1]);
+		
+		return neighborhood;
+		
+	}
 
 	public ArrayList<Fish> getFishAlive() {
 		return fishAlive;
@@ -70,8 +118,5 @@ public class Sea {
 	public void setFishAlive(ArrayList<Fish> fishAlive) {
 		this.fishAlive = fishAlive;
 	}
-	
-	
     
 }
-
