c744c63df37c9b4c05c91d4c615bf0455dc0afe7
plabadille <plabadille@gmail.com>
2017-01-08 18:17:24 +0100

    First launchable version of the game

--- model/gameEngine/LifeGame.java
+++ model/gameEngine/LifeGame.java
@@ -1,65 +1,88 @@
+/**
+ * A class for lauching the game and stop it when it's done
+ * The game end when just one species or no species still alive
+ * @author Pierre Labadille, Alexandre Ducreux
+ * @since 2016-12-01
+ */
+
 package model.gameEngine;
+
+import java.util.ArrayList;
 
 import model.fish.*;
 import model.sea.*;
 
 public class LifeGame {
 	
-	protected Sea sea;
-	protected GameConstants gameConstants;
-	protected int round;
+	private Sea sea;
+	private GameConstants gameConstants;
+	private int round;
+	private boolean twoSpeciesALive;
+	private String winnerSpecies;
 	
 	public LifeGame() {
 		this.gameConstants = new GameConstants();
 		this.sea = new Sea(this.gameConstants);
 		this.round = 0;
+		this.twoSpeciesALive = true;
 	}
 	
-	protected void runTime(int nr) {
-		for (int i = 0; i < nr; i++) {
-			playRound();
-		}
-	}
-	
-	protected void runTimeUntilNoSpeciesAlive() {
-		boolean twoSpecies = true;
-		while(twoSpecies) {
-			playRound();
-			
-			//check if no species alive
-			if (sea.getFishAlive().isEmpty()) {
-				twoSpecies = false;
-				break;
+	public void quickRunGame() {
+		while(this.twoSpeciesALive) {
+			if (this.checkIfTwoSpeciesAlive()) {
+				this.playRound();
 			}
 		}
+		this.endGame();
 	}
 	
-	protected void runTimeUntilOneSpeciesAlive() {
-		boolean twoSpecies = true;
-		while(twoSpecies) {
-			playRound();
-			
-			//check if only one species survive
-			boolean sardineAlive = false;
-			boolean sharkAlive = false;
-			for(Fish fish : sea.getFishAlive()) {
-				if (fish instanceof Sardine) {
-					sardineAlive = true;
-				}
-				if (fish instanceof Shark) {
-					sharkAlive = true;
-				}
-			}
-			if (!sharkAlive || !sardineAlive) {
-				twoSpecies = false;
-				break;
+	public void runGame() {
+		while(this.twoSpeciesALive) {
+			if (this.checkIfTwoSpeciesAlive()) {
+				this.playRound();
 			}
 		}
+		this.endGame();
 	}
 	
-	protected void playRound() {
+	private boolean checkIfTwoSpeciesAlive() {
+		ArrayList<Fish> fishAlive = this.sea.getFishAlive();
+		boolean sardineAlive = false;
+		boolean sharkAlive = false;
+		
+		for (Fish fish : fishAlive) {
+			if (fish instanceof Sardine && !sardineAlive) {
+				sardineAlive = true;
+			} else if (fish instanceof Shark && !sharkAlive) {
+				sharkAlive = true;
+			}
+			//if at least one of each species is alive, we don't need to keep running the loop.
+			if (sardineAlive && sharkAlive) {
+				return true;
+			}
+		}
+		this.winnerSpecies = sardineAlive ? "sardines" : sharkAlive ? "sharks" : null;
+		this.twoSpeciesALive = false;
+		return false;
+	}
+
+	private void playRound() {
 		this.round++;
-		//to do
+		
+		ArrayList<Fish> fishAlive = this.sea.getFishAlive();
+		for (Fish fish : fishAlive) {
+			fish.playRound(this.sea);
+		}
+	}
+	
+	private void endGame() {
+		System.out.println("The game ended in " + this.round + " game cycle");
+		if (this.winnerSpecies != null) {
+			System.out.println("The species who survive is the  " + this.winnerSpecies + "!");
+		} else {
+			System.out.println("No species survive... They both die in the same cycle.");
+		}
+		
 	}
 
 }
